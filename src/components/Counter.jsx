219c35242f092457fc9ff7d3aaa3f1f41de119a9
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-04-17 23:36:02 +0530

    Counter cmp using jsx

---
+++ src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [data, setData] = useState(0);
+  const inc = () => {
+    setData(data + 1);
+  };
+  const dec = () => {
+    setData(data - 1);
+  };
+  return (
+    <>
+      <h1>{data}</h1>
+      <button onClick={inc}>INCREMENT</button>
+      <button onClick={dec}>DECREMENT</button>
+    </>
+  );
+};
+export default Counter;
